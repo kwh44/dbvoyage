568c723802348d43fcf91421db3aedc074b4a814
kwh44 <maistruk44@gmail.com>
2019-11-28 14:47:01 +0200

    Add decompression message

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 if [ "$1" == "download" ]; then
   mkdir dumps -p
   cd dumps && wget https://dumps.wikimedia.org/enwikivoyage/latest/enwikivoyage-latest-pages-articles.xml.bz2
+  echo "Decompressing dump archive. Please, wait ..."
   bzip2 -d enwikivoyage-latest-pages-articles.xml.bz2
   cd ..
 fi
