f21de15f03c53aee32255c4f09b190a6037e1d91
kwh44 <maistruk44@gmail.com>
2019-12-01 15:12:33 +0200

    Add regionslist extractor

--- extraction/regions_section_extractor.hpp
+++ extraction/regions_section_extractor.hpp
@@ -9,6 +9,7 @@
 #include "../utils/listing_parameters.hpp"
 #include "../dump_parser/parser.hpp"
 #include "extractor_base.hpp"
+#include <boost/algorithm/string/trim.hpp>
 
 class RegionsSectionExtractor : public Extractor {
     using regions_list=std::shared_ptr<std::vector<std::string>>;
@@ -20,18 +21,33 @@ public:
 
     void extract() override {
         for (auto it = articles->begin(); it != PageNode::end(); ++it) {
+
             if (PageNode::na_article(it)) continue;
             if (PageNode::redirect_article(it)) continue;
+
             std::string page_title(PageNode::get_page_title(it));
+
+
             auto page_text_itr = PageNode::get_text(it);
+
             while (start_find(page_text_itr, "{{Region")) {
+
                 auto tag_size = find_regions_tag_size(page_text_itr);
                 auto regions = get_region_names(std::string(page_text_itr, tag_size));
-                for (const auto &region: *regions) {
+
+                for (auto &region: *regions) {
+
+                    boost::algorithm::trim(region);
+                    boost::algorithm::trim(page_title);
+                    clean_url(region);
+                    clean_url(page_title);
+
                     std::string subject("<https://dbvoyage.org/ontology/article/" + page_title + ">");
                     std::string predicate("<https://dbvoyage.org/ontology/property/hasRegion>");
                     std::string object("<https://dbvoyage.org/ontology/article/" + region + '>');
+
                     create_statement(subject, predicate, object);
+
                 }
                 ++page_text_itr;
             }
@@ -42,8 +58,7 @@ private:
     static size_t find_regions_tag_size(page_iterator_t itr) {
         size_t size = 0;
         while (*itr != '\0') {
-            if (*itr == '\n' && *(itr + 1) == '}' && *(itr + 2) == '}') {
-                size += 3;
+            if (*itr == '}' && *(itr + 1) == '}' && *(itr + 2) == '\n' && *(itr + 3) == '\n') {
                 break;
             }
             ++size;
@@ -54,14 +69,26 @@ private:
 
     static regions_list get_region_names(std::string &&regions) {
         auto ptr = std::make_shared<std::vector<std::string>>();
+
         auto i = regions.find("name");
-        for (; i != std::string::npos; i = regions.find("name", i + 1)) {
+        auto j = regions.find("region", i - 9);
+
+        while (i != std::string::npos && j != std::string::npos && i - j < 10) {
+
             auto region_name_start = regions.find("[[", i);
             auto region_name_end = regions.find("]]", i);
+
             auto region_name = regions.substr(region_name_start + 2, region_name_end - region_name_start - 2);
+
             auto pos = region_name.find('|');
+
             if (pos != std::string::npos) region_name.erase(pos, region_name.size() - pos);
+
             ptr->emplace_back(region_name);
+
+            i = regions.find("name", i + 1);
+            j = regions.find("region", i - 9);
+
         }
         return ptr;
     }
