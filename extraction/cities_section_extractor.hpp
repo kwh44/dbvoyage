cc4d959fd1baf1a02a2d12dfdbb0686b0ff1b65e
kwh44 <maistruk44@gmail.com>
2019-12-01 15:49:40 +0200

    Add cities extractor

--- extraction/cities_section_extractor.hpp
+++ extraction/cities_section_extractor.hpp
@@ -6,6 +6,7 @@
 #define DBVOYAGE_CITIES_SECTION_EXTRACTOR_HPP
 
 #include <memory>
+#include <boost/algorithm/string/trim.hpp>
 #include "../utils/listing_parameters.hpp"
 #include "../dump_parser/parser.hpp"
 #include "extractor_base.hpp"
@@ -27,34 +28,38 @@ public:
             auto page_text_itr = PageNode::get_text(it);
             auto cities_list = get_cities_list(page_text_itr);
             if (cities_list == nullptr) continue;
-            for (const auto &city_name: *cities_list) {
+            for (auto &city_name: *cities_list) {
+                boost::algorithm::trim(page_title);
+                boost::algorithm::trim(city_name);
+                clean_url(page_title);
+                clean_url(city_name);
                 std::string subject("<https://dbvoyage.org/ontology/article/" + page_title + ">");
                 std::string predicate("<https://dbvoyage.org/ontology/property/hasCity>");
                 std::string object("<https://dbvoyage.org/ontology/article/" + city_name + '>');
                 create_statement(subject, predicate, object);
-                ++page_text_itr;
             }
         }
     }
 
 private:
     static cities_list get_cities_list(page_iterator_t &itr) {
-        bool flag = start_find(itr, "==Cities==");
-        if (flag == 0) return nullptr;
-        size_t end = 0;
-        while (*(itr + end) != '\n' || *(itr + 1 + end) != '\n') ++end;
-        std::string cities(itr, end);
         auto list = std::make_shared<std::vector<std::string>>();
-        std::vector<std::string> city_name_keys{"name=", "name ="};
-        for (const auto &key: city_name_keys) {
-            for (size_t i = cities.find(key); i != std::string::npos; i = cities.find(key, i + 1)) {
-                auto city_name = cities.substr(i + key.size(), cities.find('|', i) - i - key.size());
-                auto link = city_name.find('[');
-                if (link != std::string::npos) city_name.erase(0, 2);
-                link = city_name.find(']');
-                if (link != std::string::npos) city_name.erase(city_name.size() - 2, 2);
-                list->emplace_back(city_name);
-            }
+
+        bool flag = start_find(itr, "type=city");
+
+        while (flag) {
+
+            while (*itr != '[') ++itr;
+
+            auto end = itr;
+            while (*end != ']') ++end;
+
+            std::string city_name(itr+2, end);
+
+            list->emplace_back(city_name);
+
+            ++itr;
+            flag = start_find(itr, "type=city");
         }
         return list;
     }
