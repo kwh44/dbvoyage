0acddd3160e01e3f5d80a7fa2e1b8067016dd072
kwh44 <maistruk44@gmail.com>
2019-12-07 19:19:45 +0200

    Change server.js for node view

--- server/server.js
+++ server/server.js
@@ -1,9 +1,48 @@
 let http = require("http");
 let fs = require("fs");
-let query_graph = require("./static/js/sparql_query");
+
+const graph = "http://ec2-52-15-243-121.us-east-2.compute.amazonaws.com";
+const port_number = 8081;
+
+let sendFile = (file, res) => {
+    file.pipe(res);
+    file.on('error', function (err) {
+        console.err(err);
+        file.destroy();
+    });
+    res.on('close', function () {
+        file.destroy();
+    });
+};
+
+let query_graph = (query) => {
+    let fetch = require("node-fetch");
+    const endpoint = "http://localhost:8890/sparql";
+    const url = endpoint +
+        '?query=' + encodeURIComponent(query) +
+        '&format=json' +
+        '&default-graph-uri=' + encodeURIComponent(graph) +
+        '&format=text/html' +
+        '&timeout=0';
+    return fetch(url);
+};
+
+let process_query = (query, res) => {
+    query_graph(query).then(res => res.text()).then(html => {
+        if (html.substr(1, 5) === "table") {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write("<html lang=\"en\"><body>" + html + "</body></html>");
+            res.end();
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            let file = new fs.ReadStream('static/routes/invalid_query.html');
+            sendFile(file, res);
+        }
+    });
+};
 
 let server = new http.Server();
-const port_number = 8081;
+
 server.listen(port_number, "127.0.0.1");
 
 server.on("request", function (req, res) {
@@ -28,36 +67,11 @@ server.on("request", function (req, res) {
                     process_query(query, res);
                 });
             } else {
-                let query = "select ?predicate ?object where { <" + req.url + "> ?predicate ?object}";
+                let query = "select ?predicate ?object where { <" + graph + req.url + "> ?predicate ?object}";
                 process_query(query, res);
             }
         }
     }
 );
-
-let sendFile = (file, res) => {
-    file.pipe(res);
-    file.on('error', function (err) {
-        console.err(err);
-        file.destroy();
-    });
-    res.on('close', function () {
-        file.destroy();
-    });
-};
-
-let process_query = (query, res) => {
-    query_graph(query).then(res => res.text()).then(html => {
-        if (html.substr(1, 5) === "table") {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write("<html lang=\"en\"><body>" + html + "</body></html>");
-            res.end();
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            let file = new fs.ReadStream('static/routes/invalid_query.html');
-            sendFile(file, res);
-        }
-    });
-};
 
 process.stdout.write("Server is running.\nGo to http://localhost:" + port_number + ".\n");
